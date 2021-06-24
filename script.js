e05632d5077d07caa08f0d7f98b6a474bfab63c4
lilsson <pirckhelavalile@gmail.com>
2021-06-24 17:32:54 +0300

    add script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// აიღეთ თვითოეული რიცხვი სტრინგიდან
+let numb = "123456789"
+
+for(let numb = 1; numb < 10; numb ++){
+    console.log(numb)
+}
+
+// გაადაქციეთ რიცხვად და გადაამრავლეთ თავის თავზე
+let numb = 123456789
+
+for(let numb = 1; numb < 10; numb ++){
+    console.log(numb)
+}
+   if(numb % 9 === 0){
+       break;
+   }
